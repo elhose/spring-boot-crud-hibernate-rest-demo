248f53e5a02e4a95a7e75f96812490b2d38619aa
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-01-13 10:23:54 +0100

    spring boot hibernate crud rest

---
+++ src/main/java/tt/psc/springboot/crudhibernaterestdemo/CrudHibernateRestDemoApplication.java
@@ -0,0 +1,13 @@
+package tt.psc.springboot.crudhibernaterestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudHibernateRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudHibernateRestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tt/psc/springboot/crudhibernaterestdemo/CrudHibernateRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tt.psc.springboot.crudhibernaterestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudHibernateRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
